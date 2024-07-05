77b8c892afce4b4cac003b77dfb5f4592a712f7f
Azizul Islam <alifalamin42@gmail.com>
2024-07-05 21:59:09 +0600

    todo modal added

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -1,16 +1,22 @@
 import { Button } from "../ui/button";
+import { MdDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
 
 const TodoCard = () => {
   return (
     <div>
-      <div className=" bg-white rounded-md  flex justify-between items-center p-4">
+      <div className=" bg-slate-100 rounded-md  flex justify-between items-center p-4 border">
         <input type="checkbox" name="" id="" />
         <p className="font-bold ">Todo Title</p>
         <p>Time</p>
         <p>Description</p>
         <div className="space-x-4">
-          <Button>Delete</Button>
-          <Button>Edit</Button>
+          <Button className="bg-red-500 font-semibold text-xl">
+            <MdDelete />
+          </Button>
+          <Button className="bg-blue-500 font-semibold text-xl">
+            <FaEdit />
+          </Button>
         </div>
       </div>
     </div>
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,15 +1,28 @@
-import { Button } from "../ui/button";
 import TodoCard from "./TodoCard";
+import TodoFilter from "./TodoFilter";
+import TodoModal from "./TodoModal";
 
 const TodoContainer = () => {
   return (
     <div>
-      <div className="space-x-4 text-center mb-2">
-        <Button>Add Todo</Button>
-        <Button>Filter</Button>
+      <div className="space-x-4 mb-5 flex justify-between">
+        <TodoModal />
+        <TodoFilter />
+        {/* <Button className="bg-primary-gradient font-semibold">
+          <TodoModal />
+        </Button>
+        <Button className="bg-primary-gradient font-semibold">
+          <TodoFilter />
+        </Button> */}
       </div>
-      <div className="bg-red-500 w-full h-[500px] rounded-xl p-5">
-        <TodoCard />
+      <div className="bg-primary-gradient w-full h-full p-1 rounded-xl ">
+        {/* <div className="bg-white font-bold text-2xl p-3 flex justify-center items-center rounded-md">
+          <p>There is no task pending</p>
+        </div> */}
+        <div className="bg-white p-5 w-full h-full rounded-lg space-y-2">
+          <TodoCard />
+          <TodoCard />
+        </div>
       </div>
     </div>
   );
